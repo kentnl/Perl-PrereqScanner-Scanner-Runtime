71219d7276249932e524541c22cd4fc886c68448
Kent Fredric <kentfredric@gmail.com>
2016-04-10 06:05:02 +1200

    Ignore method calls for Class::Load

--- lib/Perl/PrereqScanner/Scanner/Class/Load.pm
+++ lib/Perl/PrereqScanner/Scanner/Class/Load.pm
@@ -22,9 +22,7 @@ my %functions = (
   try_load_class      => '_get_pn',
   load_optional_class => '_get_pn',
 );
-my %subs = (
-  map { ( "Class::Load::$_" => [ $functions{$_}, $_ ], "$_" => [ $functions{$_}, $_ ], ) } keys %functions
-);
+my %subs = ( map { ( "Class::Load::$_" => [ $functions{$_}, $_ ], "$_" => [ $functions{$_}, $_ ], ) } keys %functions );
 
 =for Pod::Coverage scan_for_prereqs
 
@@ -47,12 +45,18 @@ sub scan_for_prereqs {
     sub {
       return q[] unless $_[1]->isa('PPI::Statement');
       my (@children) = $_[1]->schildren;
-      while ( my $child = shift @children ) {
+      my ( $child, $previous );
+      while (@children) {
+        $previous = $child;
+        $child    = shift @children;
 
         # Match sub call
         next
           unless $child->isa('PPI::Token::Word')
           and $child->literal =~ qr/$literal_re/sx;
+
+        # Skip methods
+        next if $previous and $previous->isa('PPI::Token::Operator') and q{->} eq $previous->content;
 
         # Handle a list of arguments as token->next
         if ( $children[0]->isa('PPI::Structure::List') ) {
